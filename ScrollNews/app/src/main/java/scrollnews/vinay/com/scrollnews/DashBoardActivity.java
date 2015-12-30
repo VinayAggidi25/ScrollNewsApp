8f1fbf4f2ab7e4d8d1179720f76b0a71ca98ce88
vinayaggidi <avinay@novopay.in>
2015-12-30 19:15:53 +0530

    modified login using google

---
+++ ScrollNews/app/src/main/java/scrollnews/vinay/com/scrollnews/DashBoardActivity.java
@@ -0,0 +1,16 @@
+package scrollnews.vinay.com.scrollnews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by vinayaggidi on 30-12-2015.
+ */
+public class DashBoardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
